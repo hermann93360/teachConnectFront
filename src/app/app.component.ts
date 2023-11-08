578992505e8fe5c52aeb7ed47f94b40809a82110
hermann <hkamguin@gmail.com>
2023-11-08 17:57:44 +0100

    follow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Relaschool';
+  loginForm: boolean = true;
+  signupForm: boolean = false;
+
+  showLoginForm(){
+    this.loginForm = true;
+    this.signupForm = false;
+  }
+
+  showSignupForm(){
+    this.loginForm = false;
+    this.signupForm = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { SingupComponent } from './singup/singup.component';
+import {MatRadioModule} from "@angular/material/radio";
+import {RouterModule, Routes} from "@angular/router";
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  {
+    path: '', // Chemin vide pour la page d'accueil
+    component: LoginComponent,
+  },
+  {
+    path: 'sign',
+    component: SingupComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SingupComponent,
+    DashboardComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatRadioModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
